63fd104105b862b494f9c57df2dae27151ba7007
黎摄文 <lishewen@gmail.com>
2020-09-27 05:14:42 +0800

    Init

---
+++ JT808.Protocol.Extensions.Streamax/JT808_Streamax_Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JT808.Protocol.Extensions.Streamax
+{
+    public static class JT808_Streamax_Constants
+    {
+        /// <summary>
+        /// 运营登记
+        /// </summary>
+        public const uint JT808_0x0B01 = 0x0B01;
+        /// <summary>
+        /// 到离站信息上报
+        /// </summary>
+        public const uint JT808_0x0B02 = 0x0B02;
+    }
+}
---
+++ JT808.Protocol.Extensions.Streamax/StreamaxDependencyInjectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace JT808.Protocol.Extensions.Streamax
+{
+    public static class StreamaxDependencyInjectionExtensions
+    {
+        public static IJT808Builder AddJTActiveSafetyConfigure(this IJT808Builder jT808Builder)
+        {
+            jT808Builder.Config.Register(Assembly.GetExecutingAssembly());
+            return jT808Builder;
+        }
+    }
+}
