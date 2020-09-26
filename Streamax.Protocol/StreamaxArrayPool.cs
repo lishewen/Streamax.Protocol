4376993aab82a8ec5b01553ec05e6ded2efbdb21
黎摄文 <lishewen@gmail.com>
2020-09-27 04:52:30 +0800

    添加项目文件。

---
+++ Streamax.Protocol/StreamaxArrayPool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Buffers;
+
+namespace Streamax.Protocol
+{
+    internal static class StreamaxArrayPool
+    {
+        private readonly static ArrayPool<byte> ArrayPool;
+
+        static StreamaxArrayPool()
+        {
+            ArrayPool = ArrayPool<byte>.Create();
+        }
+
+        public static byte[] Rent(int minimumLength)
+        {
+            return ArrayPool.Rent(minimumLength);
+        }
+
+        public static void Return(byte[] array, bool clearArray = false)
+        {
+            ArrayPool.Return(array, clearArray);
+        }
+    }
+}
