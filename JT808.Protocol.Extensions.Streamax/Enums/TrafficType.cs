09e7ee0aaf11cb7087c9f1da733dcac8af36d9a2
算神 <lishewen@msn.com>
2020-09-27 08:56:49 +0800

    到离站信息上报

---
+++ JT808.Protocol.Extensions.Streamax/Enums/TrafficType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JT808.Protocol.Extensions.Streamax.Enums
+{
+    /// <summary>
+    /// 业务类型
+    /// </summary>
+    public enum TrafficType
+    {
+        上行 = 0x01,
+        下行 = 0x02,
+        环行 = 0x03,
+        停主站 = 0x04,
+        停副站 = 0x05,
+        出场 = 0x80,
+        进场 = 0x81,
+        加油 = 0x82,
+        加气 = 0x83,
+        充电 = 0x84,
+        小修 = 0x85,
+        大修 = 0x86,
+        一保 = 0x87,
+        二保 = 0x88,
+        三保 = 0x89,
+        放空 = 0x8A,
+        停场 = 0x8B
+    }
+}
--- JT808.Protocol.Extensions.Streamax/MessageBody/JT808_0x0B02.cs
+++ JT808.Protocol.Extensions.Streamax/MessageBody/JT808_0x0B02.cs
@@ -1,4 +1,5 @@
-﻿using JT808.Protocol.Formatters;
+﻿using JT808.Protocol.Extensions.Streamax.Enums;
+using JT808.Protocol.Formatters;
 using JT808.Protocol.Interfaces;
 using JT808.Protocol.MessagePack;
 using System;
@@ -24,19 +25,150 @@ namespace JT808.Protocol.Extensions.Streamax.MessageBody
         /// 到离站类型
         /// </summary>
         public byte PointType { get; set; }
+        /// <summary>
+        /// 业务类型
+        /// </summary>
+        public byte TrafficType { get; set; }
+        /// <summary>
+        /// 场站站点编号
+        /// </summary>
+        public uint StationId { get; set; }
+        /// <summary>
+        /// 车站序号
+        /// </summary>
+        public byte StationNo { get; set; }
+        /// <summary>
+        /// 标志字段
+        /// 0位：0：自动触发；1：手动触发
+        /// 1位：0：首次发送；1：补发
+        /// 2位：0：未定位；1：定位
+        /// 3-7位：保留
+        /// </summary>
+        public byte Tag { get; set; }
+        /// <summary>
+        /// 纬度
+        /// 以度位单位的纬度值乘以10^6，精确到百万分之一度
+        /// </summary>
+        public uint Latitude { get; set; }
+        /// <summary>
+        /// 经度
+        /// 以度位单位的经度值乘以10^6，精确到百万分之一度
+        /// </summary>
+        public uint Longitude { get; set; }
+        /// <summary>
+        /// 高程
+        /// </summary>
+        public ushort Altitude { get; set; }
+        /// <summary>
+        /// 车速
+        /// </summary>
+        public ushort Speed { get; set; }
+        /// <summary>
+        /// 方向
+        /// </summary>
+        public ushort Direction { get; set; }
+        /// <summary>
+        /// 时间
+        /// YYMMDDhhmmss
+        /// BCD[6]
+        /// </summary>
+        public DateTime Time { get; set; }
+        /// <summary>
+        /// 当前乘客数
+        /// </summary>
+        public ushort PersonCount { get; set; }
+        /// <summary>
+        /// 车门数
+        /// </summary>
+        public byte DoorCount { get; set; }
+        /// <summary>
+        /// 乘客计数项列表
+        /// 长度n*3
+        /// 0:门编号
+        /// 1:上客数
+        /// 2:下客数
+        /// </summary>
+        public byte[] DoorData { get; set; }
         public void Analyze(ref JT808MessagePackReader reader, Utf8JsonWriter writer, IJT808Config config)
         {
-            throw new NotImplementedException();
+            JT808_0x0B02 value = new JT808_0x0B02();
+            value.GprsId = reader.ReadUInt32();
+            writer.WriteNumber($"[{value.GprsId.ReadNumber()}]线路编号", value.GprsId);
+            value.PointType = reader.ReadByte();
+            writer.WriteNumber($"[{value.PointType.ReadNumber()}]到离站类型-{Enum.GetName(typeof(PointType), value.PointType)}", value.PointType);
+            value.TrafficType = reader.ReadByte();
+            writer.WriteNumber($"[{value.TrafficType.ReadNumber()}]业务类型-{Enum.GetName(typeof(TrafficType), value.TrafficType)}", value.TrafficType);
+            value.StationId = reader.ReadUInt32();
+            writer.WriteNumber($"[{value.StationId.ReadNumber()}]场站站点编号", value.StationId);
+            value.StationNo = reader.ReadByte();
+            writer.WriteNumber($"[{value.StationNo.ReadNumber()}]车站序号", value.StationNo);
+            value.Tag = reader.ReadByte();
+            writer.WriteNumber($"[{value.Tag.ReadNumber()}]标志字段", value.Tag);
+            value.Latitude = reader.ReadUInt32();
+            writer.WriteNumber($"[{value.Latitude.ReadNumber()}]纬度", value.Latitude);
+            value.Longitude = reader.ReadUInt32();
+            writer.WriteNumber($"[{value.Longitude.ReadNumber()}]经度", value.Longitude);
+            value.Altitude = reader.ReadUInt16();
+            writer.WriteNumber($"[{value.Altitude.ReadNumber()}]高程", value.Altitude);
+            value.Speed = reader.ReadUInt16();
+            writer.WriteNumber($"[{value.Speed.ReadNumber()}]车速", value.Speed);
+            value.Direction = reader.ReadUInt16();
+            writer.WriteNumber($"[{value.Direction.ReadNumber()}]方向", value.Direction);
+            value.Time = reader.ReadDateTime6();
+            writer.WriteString($"[{value.Time:yyMMddHHmmss}]时间", value.Time.ToString("yyyy-MM-dd HH:mm:ss"));
+            value.PersonCount = reader.ReadUInt16();
+            writer.WriteNumber($"[{value.PersonCount.ReadNumber()}]当前乘客数", value.PersonCount);
+            value.DoorCount = reader.ReadByte();
+            writer.WriteNumber($"[{value.DoorCount.ReadNumber()}]车门数", value.DoorCount);
+            writer.WriteStartArray("乘客计数项列表");
+            for (int i = 0; i < value.DoorCount; i++)
+            {
+                writer.WriteStartObject();
+                var idBuffer = reader.ReadArray(3).ToArray();
+                writer.WriteString($"Id{i + 1}", idBuffer.ToHexString());
+                writer.WriteEndObject();
+            }
+            writer.WriteEndArray();
         }
 
         public JT808_0x0B02 Deserialize(ref JT808MessagePackReader reader, IJT808Config config)
         {
-            throw new NotImplementedException();
+            JT808_0x0B02 value = new JT808_0x0B02();
+            value.GprsId = reader.ReadUInt32();
+            value.PointType = reader.ReadByte();
+            value.TrafficType = reader.ReadByte();
+            value.StationId = reader.ReadUInt32();
+            value.StationNo = reader.ReadByte();
+            value.Tag = reader.ReadByte();
+            value.Latitude = reader.ReadUInt32();
+            value.Longitude = reader.ReadUInt32();
+            value.Altitude = reader.ReadUInt16();
+            value.Speed = reader.ReadUInt16();
+            value.Direction = reader.ReadUInt16();
+            value.Time = reader.ReadDateTime6();
+            value.PersonCount = reader.ReadUInt16();
+            value.DoorCount = reader.ReadByte();
+            value.DoorData = reader.ReadArray(value.DoorCount * 3).ToArray();
+            return value;
         }
 
         public void Serialize(ref JT808MessagePackWriter writer, JT808_0x0B02 value, IJT808Config config)
         {
-            throw new NotImplementedException();
+            writer.WriteUInt32(value.GprsId);
+            writer.WriteByte(value.PointType);
+            writer.WriteByte(value.TrafficType);
+            writer.WriteUInt32(value.StationId);
+            writer.WriteByte(value.StationNo);
+            writer.WriteByte(value.Tag);
+            writer.WriteUInt32(value.Latitude);
+            writer.WriteUInt32(value.Longitude);
+            writer.WriteUInt16(value.Altitude);
+            writer.WriteUInt16(value.Speed);
+            writer.WriteUInt16(value.Direction);
+            writer.WriteDateTime6(value.Time);
+            writer.WriteUInt16(value.PersonCount);
+            writer.WriteByte(value.DoorCount);
+            writer.WriteArray(value.DoorData);
         }
     }
 }
