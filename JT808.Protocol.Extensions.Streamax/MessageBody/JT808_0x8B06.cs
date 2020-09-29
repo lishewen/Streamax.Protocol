06550932c169932acf629c1774417f7085f3a50e
黎摄文 <lishewen@gmail.com>
2020-09-30 05:33:45 +0800

    校时

---
+++ JT808.Protocol.Extensions.Streamax/MessageBody/JT808_0x8B06.cs
@@ -0,0 +1,44 @@
+﻿using JT808.Protocol.Formatters;
+using JT808.Protocol.Interfaces;
+using JT808.Protocol.MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace JT808.Protocol.Extensions.Streamax.MessageBody
+{
+    /// <summary>
+    /// 校时应答
+    /// </summary>
+    public class JT808_0x8B06 : JT808Bodies, IJT808MessagePackFormatter<JT808_0x8B06>, IJT808Analyze
+    {
+        public override ushort MsgId => 0x8B06;
+
+        public override string Description => "校时应答";
+        /// <summary>
+        /// 时间
+        /// YYMMDDhhmmss
+        /// BCD[6]
+        /// </summary>
+        public DateTime Time { get; set; }
+        public void Analyze(ref JT808MessagePackReader reader, Utf8JsonWriter writer, IJT808Config config)
+        {
+            JT808_0x8B06 value = new JT808_0x8B06();
+            value.Time = reader.ReadDateTime6();
+            writer.WriteString($"[{value.Time:yyMMddHHmmss}]时间", value.Time.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+
+        public JT808_0x8B06 Deserialize(ref JT808MessagePackReader reader, IJT808Config config)
+        {
+            JT808_0x8B06 value = new JT808_0x8B06();
+            value.Time = reader.ReadDateTime6();
+            return value;
+        }
+
+        public void Serialize(ref JT808MessagePackWriter writer, JT808_0x8B06 value, IJT808Config config)
+        {
+            writer.WriteDateTime6(value.Time);
+        }
+    }
+}
