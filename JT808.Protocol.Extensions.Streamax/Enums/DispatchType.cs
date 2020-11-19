4765e8db6dec9fce0ddeb8df8e8b18db9d59ec64
算神 <lishewen@msn.com>
2020-11-19 09:44:42 +0800

    0x8B01 发车通知

---
+++ JT808.Protocol.Extensions.Streamax/Enums/DispatchType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JT808.Protocol.Extensions.Streamax.Enums
+{
+    /// <summary>
+    /// 调度类型
+    /// </summary>
+    public enum DispatchType
+    {
+        全程 = 0x01,
+        区间 = 0x02,
+        放站 = 0x03
+    }
+}
