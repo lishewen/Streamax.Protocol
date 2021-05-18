07590736ddc99ee6451d21f4b1fe92acd81423a1
算神 <lishewen@msn.com>
2021-05-18 16:17:48 +0800

    运营登记单元测试

---
+++ JT808.Protocol.Extensions.Streamax.Test/JT808_0x0B01_Test.cs
@@ -0,0 +1,24 @@
+using System;
+using JT808.Protocol.Extensions.Streamax.MessageBody;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace JT808.Protocol.Extensions.Streamax.Test
+{
+    public class JT808_0x0B01_Test
+    {
+        JT808Serializer JT808Serializer;
+        public JT808_0x0B01_Test()
+        {
+            ServiceCollection serviceDescriptors = new ServiceCollection();
+            serviceDescriptors.AddJT808Configure().AddStreamaxConfigure();
+            IJT808Config jT808Config = serviceDescriptors.BuildServiceProvider().GetRequiredService<IJT808Config>();
+            JT808Serializer = new JT808Serializer(jT808Config);
+        }
+        [Fact]
+        public void Json()
+        {
+            var json = JT808Serializer.Analyze("7e0b01000d0000000007920f4c000000dc656530343736643500597e".ToHexBytes());
+        }
+    }
+}
